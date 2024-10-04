c95d32f169690880c95f03df2476ebfb4a762edf
gobbez <a.gobbez@hotmail.it>
2024-10-04 17:49:21 +0200

    Perfected bot code to loop and search position

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Lichess Bot for the purpose of studying both chess and Deep Learning.. and chall
     <td>
       <h1>Main Features</h1>
       <ul>
-        <li>98+ millions chess positions powered by Stockfish (working)</li>
+        <li>98+ millions chess positions powered by Stockfish, divided by 4 categories (100k - 2m - 10m - 98m positions) (working)</li>
         <li>Different Deep Learning models on its sleeve (not used due to low %)</li>
         <li>Ollama model to chat, interact and even teach user in the game (added soon!)</li>
       </ul>
@@ -22,11 +22,15 @@ Lichess Bot for the purpose of studying both chess and Deep Learning.. and chall
 ## Info
 This Chess Bot will have different styles. 
 
-### Stockfish 98 million positions (Working)
+### Stockfish million positions: 0.1m - 2m - 10m - 98m (Working)
 Analyze up to 98 millions chess positions with FENs, evals and best move and create a pandas DataFrame.
 This file uses the Lichess Evaluations .zst file that has up to 98 millions different fens with Stockfish evaluations and best move.
 My code will loop through it and extract Fen, Eval and Move (first best move).
-My Bot will search the current position in the created DataFrame and find the best move (download link in the repo)
+My Bot will search the current position in the created DataFrame and find the best move (download link in the repo).
+
+Update: Now there are 4 datasets (respectively with 100k, 2m, 10m, 98m)
+Now my Bot will use them based on opponent Elo (but 98m is too slow so it's not using it for real). 
+Check "Performances" below to see search speed of each one.
 
 
 ### Deep Learning 2 million positions (too low %)
@@ -43,15 +47,59 @@ This is working, but the training is veeery slow (more than 5 hours) and for the
 I'm stopping this, but you can find the code in the repo.
 
 
+### My own chess logic (trying to create a logic from fen - work in progress!)
+
+Since the most of the positions of all these databases are from endgames or finished matches, the bot often fails to get the move.
+To solve this i'll try to implement my own chess logic-human-like, from my experience in chess game (and the help of Stockfish too) from fen.
+
+
 ### Ollama Model to Chat, Interact and Teach Users (work in progress!)
 
 The Bot will be powered by Ollama models (maybe Gemma2b) to chat with the user, analyzing position and giving hints.
 Nope, even if it wants to teach you, it wants to win at all costs too!
 
 
+#### Performances
+Here are the performances (aka time to execute that part of code) for the 4 possible databases:
+Load time is the first time the dataframe is loaded by pandas (only the first time the code is loaded).
+Search time it's the time spent to loop searching for FEN and extract best move.
+
+Performances are done searching a starting position with my pc: Intel(R) Core(TM) i7-10750H CPU @ 2.60GHz - 16 Gb Ram
+Please note that these times can become a lot more longer in a real game in a complicated (or rare) position when the bot is online.
+
+<table>
+  <tr>
+    <th>Dataset</th>
+    <th>Load Time</th>
+    <th>Search Time</th>
+  </tr>
+  <tr>
+    <td>100k positions</td>
+    <td>0.1 seconds</td>
+    <td>0.6 seconds</td>
+  </tr>
+  <tr>
+    <td>2m positions</td>
+    <td>2.2 seconds</td>
+    <td>0.9 seconds</td>
+  </tr>
+  <tr>
+    <td>10m positions</td>
+    <td>10.9 seconds</td>
+    <td>2.2 seconds</td>
+  </tr>
+  <tr>
+    <td>98m positions</td>
+    <td>8.9 minutes</td>
+    <td>6.5 minutes</td>
+  </tr>
+</table>
+
+
+
 #### Current version
 
-For now it uses Stockfish 98 million positions to recognize and make best move and, if the position isn't found (is it possible?), it just does a random move.
+For now it uses one of the 4 datasets based on opponent level in order to recognize and make best move and, if the position isn't found (is it possible?), it just does a random move.
 Working to make it improve!
 
 
--- bot/run_bot.py
+++ bot/run_bot.py
@@ -5,8 +5,29 @@ import random
 import pandas as pd
 import threading
 import time
+from pathlib import Path
 
-import read_fen_database
+# Load df
+THIS_FOLDER = Path(__file__).parent.resolve()
+start_time = time.time()
+path_df_100k = THIS_FOLDER / "database/100thousandsmoves.csv"
+df_move_100k = pd.read_csv(path_df_100k)
+print(f'Loaded df_move_100k in {round(time.time() - start_time)} s')
+start_time = time.time()
+path_df_2m = THIS_FOLDER / "database/2millionmoves.csv"
+df_move_2m = pd.read_csv(path_df_2m)
+print(f'Loaded df_move_2m in {round(time.time() - start_time)} s')
+start_time = time.time()
+path_df_10m = THIS_FOLDER / "database/10millionmoves.csv"
+df_move_10m = pd.read_csv(path_df_10m)
+print(f'Loaded df_move_10m in {round(time.time() - start_time)} s')
+""" Comment since 98 millions parameter is too slow
+start_time = time.time()
+path_df_98m = THIS_FOLDER / "database/98millionmoves.csv"
+df_move_98m = pd.read_csv(path_df_98m)
+print(f'Loaded path_df_98m in {round(time.time() - start_time)} s')"""
+# List of active games id:
+list_playing_id = []
 
 
 # Load configuration from file config.yml
@@ -18,16 +39,46 @@ session = berserk.TokenSession(config['token'])
 client = berserk.Client(session=session)
 
 
-def handle_game_bot_turn(game_id, fen):
+def search_fen_from_csv(df_moves, fen):
+    """
+    Search the best move from the csv file passing its fen
+    :param df_moves: DataFrame to pass
+    :param fen: FEN position of the game
+    :return: best move
+    """
+    # Search for FEN
+    parts = fen.split(' ')
+    search_fen = ' '.join(parts[:2])
+
+    # Loop and get only first match
+    count_search = 0
+    count_thousands = 0
+    for index, row in df_moves.iterrows():
+        count_search += 1
+        if count_search > 9999:
+            count_thousands += 1
+            print(count_thousands * 10000)
+            count_search = 0
+
+        if search_fen in row['Fen']:
+            return row['Move']
+
+    # If no match is found, return None
+    return None
+
+
+def handle_game_bot_turn(game_id, fen, elo_opponent):
     """
     This function handles the moves on Lichess and saves moves.
     :param game_id: the game id that the bot is currently playing
     :param fen: the fen position, to pass to read_fen_database to extract move
+    :param elo_opponent: opponent Lichess elo
     """
     chess_board = chess.Board()
     move_number = 1  # Initialize move number
 
     for event in client.bots.stream_game_state(game_id):
+        print(event['id'])
         if 'state' not in event:
             continue  # Skip this event if it doesn't contain the 'state' key
 
@@ -49,11 +100,10 @@ def handle_game_bot_turn(game_id, fen):
         # In case white has a move more, meaning it's black turn, update it with an x
         if len(list_white) != len(list_black):
             list_black.append('x')
-        # Save moves in a DataFrame
-        df_moves = pd.DataFrame({'white': list_white, 'black': list_black})
-        print(df_moves)
+        # Save moves in a DataFrame (not used anymore)
+        # df_moves = pd.DataFrame({'white': list_white, 'black': list_black})
 
-        if len(moves) == 0:
+        if 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR' in fen:
             # First move
             if chess_board.turn == chess.WHITE:
                 initial_move = 'g2g3'
@@ -61,21 +111,42 @@ def handle_game_bot_turn(game_id, fen):
                 initial_move = 'g7g6'
             client.bots.make_move(game_id, initial_move)
             chess_board.push_uci(initial_move)
+            print('I made first move')
         else:
-            # Search for fen
-            next_move = read_fen_database.search_fen_in_zst(fen)
-
+            # Search for fen using dataset based on opponent Elo
+            if elo_opponent < 1000:
+                next_move = search_fen_from_csv(df_move_100k, fen)
+                print(f'Searched 100k, next_move: {next_move}')
+            elif elo_opponent < 1500:
+                next_move = search_fen_from_csv(df_move_2m, fen)
+                print(f'Searched 2m, next_move: {next_move}')
+            else:
+                next_move = search_fen_from_csv(df_move_10m, fen)
+                print(f'Searched 10m, next_move: {next_move}')
             # if next_move found, then use it. Else try a random move
             if next_move:
-                client.bots.make_move(game_id, next_move.uci())
-                chess_board.push(next_move)
+                try:
+                    uci_move = chess.Move.from_uci(next_move)
+                    client.bots.make_move(game_id, uci_move.uci())
+                    print('I moved')
+                    chess_board.push(next_move)
+                    print('Move found, i moved')
+                except Exception as e:
+                    print(f"Invalid move: {e}")
+                    list_legal_moves = list(chess_board.legal_moves)
+                    rand_move = list_legal_moves[random.randint(0, len(list_legal_moves) - 1)]
+                    client.bots.make_move(game_id, rand_move.uci())
+                    chess_board.push(rand_move)
+                    print('Invalid move.. i moved random')
+
             else:
+                print('Move not found, i go random..')
                 list_legal_moves = list(chess_board.legal_moves)
                 rand_move = list_legal_moves[random.randint(0, len(list_legal_moves) - 1)]
                 client.bots.make_move(game_id, rand_move.uci())
                 chess_board.push(rand_move)
+        list_playing_id.remove(game_id)
         return
-
 
 def main():
     game_threads = []
@@ -83,37 +154,43 @@ def main():
     def handle_events():
         try:
             events = client.bots.stream_incoming_events()
-
             for event in events:
                 if event['type'] == 'challenge':
                     if not event['challenge']['rated']:
+                        print('Challenge accepted!')
                         # Accepting only unrated games for now
                         challenge_id = event['challenge']['id']
                         client.bots.accept_challenge(challenge_id)
 
                 elif event['type'] == 'gameStart':
-                    fen = event['game']['fen']
                     board_event = event['game']
-                    if board_event['isMyTurn']:
-                        game_id = event['game']['id']
-                        game_thread = threading.Thread(target=handle_game_bot_turn, args=(game_id, fen))
+                    game_id = event['game']['id']
+                    # Check if it's Bot Turn and check if the id is not there (to prevent to open multiple threads)
+                    if board_event['isMyTurn'] and game_id not in list_playing_id:
+                        list_playing_id.append(game_id)
+                        fen = event['game']['fen']
+                        elo_opponent = event['game']['opponent']['rating']
+                        print('My turn - got id, fen and elo_opponent')
+                        game_thread = threading.Thread(target=handle_game_bot_turn, args=(game_id, fen, elo_opponent))
                         game_threads.append(game_thread)
                         game_thread.start()
-
+                        print('Finish thread')
+                time.sleep(5)
                 return
-
-        except:
-            print("Rate limit exceeded. Waiting before retrying...")
+        except berserk.exceptions.ResponseError as e:
+            print(f"Rate limit exceeded: {e}. Waiting before retrying...")
             time.sleep(90)  # Wait for 90 seconds before retrying
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            time.sleep(10)  # Wait for 10 seconds before retrying
 
     while True:
+        print('While..')
         handle_events()
         # Clean up finished game threads
         game_threads = [thread for thread in game_threads if thread.is_alive()]
-        time.sleep(5)  # Adjust the sleep time as needed
-        print('Here')
+        time.sleep(40)  # Adjust the sleep time as needed
         main()
-
 
 if __name__ == "__main__":
     main()
